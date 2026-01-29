4339c5a3d76802e78949ec6c267885c3f6283eda
theweipeng <wangweipeng@apache.org>
2026-01-29 12:08:32 +0800

    remove test

--- javascript/test/fory.test.ts
+++ javascript/test/fory.test.ts
@@ -51,45 +51,40 @@ describe('fory', () => {
             expect(error.message).toBe('outofband mode is not supported now');
         }
     });
-    // todo recover xlang privitive types
-    // describe('serializer typeinfo should work', () => {
-    //     test('can serialize and deserialize primitive types', () => {
-    //         const typeinfo = Type.int8()
-    //         testTypeInfo(typeinfo, 123)
 
-    //         const typeinfo2 = Type.int16()
-    //         testTypeInfo(typeinfo2, 123)
+    test('can serialize and deserialize primitive types', () => {
+        const typeinfo = Type.int8()
+        testTypeInfo(typeinfo, 123)
 
-    //         const typeinfo3 = Type.int32()
-    //         testTypeInfo(typeinfo3, 123)
+        const typeinfo2 = Type.int16()
+        testTypeInfo(typeinfo2, 123)
 
-    //         const typeinfo4 = Type.bool()
-    //         testTypeInfo(typeinfo4, true)
+        const typeinfo3 = Type.int32()
+        testTypeInfo(typeinfo3, 123)
 
-    //         // has precision problem
-    //         // const typeinfo5 = Type.float()
-    //         // testTypeInfo(typeinfo5, 123.456)
+        const typeinfo4 = Type.bool()
+        testTypeInfo(typeinfo4, true)
 
-    //         const typeinfo6 = Type.float64()
-    //         testTypeInfo(typeinfo6, 123.456789)
+        // has precision problem
+        // const typeinfo5 = Type.float()
+        // testTypeInfo(typeinfo5, 123.456)
 
-    //         const typeinfo7 = Type.binary()
-    //         testTypeInfo(typeinfo7, new Uint8Array([1, 2, 3]), fromUint8Array(new Uint8Array([1, 2, 3])));
+        const typeinfo6 = Type.float64()
+        testTypeInfo(typeinfo6, 123.456789)
 
-    //         const typeinfo8 = Type.string()
-    //         testTypeInfo(typeinfo8, '123')
+        const typeinfo7 = Type.binary()
+        testTypeInfo(typeinfo7, new Uint8Array([1, 2, 3]), fromUint8Array(new Uint8Array([1, 2, 3])));
 
-    //         const typeinfo9 = Type.set(Type.string())
-    //         testTypeInfo(typeinfo9, new Set(['123']))
-    //     })
+        const typeinfo8 = Type.string()
+        testTypeInfo(typeinfo8, '123')
+    })
 
-    //     function testTypeInfo(typeinfo: TypeInfo, input: any, expected?: any) {
-    //         const fory = new Fory();
-    //         const serialize = fory.registerSerializer(typeinfo);
-    //         const result = serialize.deserialize(
-    //             serialize.serialize(input)
-    //         );
-    //         expect(result).toEqual(expected ?? input)
-    //     }
-    // })
+    function testTypeInfo(typeinfo: TypeInfo, input: any, expected?: any) {
+        const fory = new Fory();
+        const serialize = fory.registerSerializer(typeinfo);
+        const result = serialize.deserialize(
+            serialize.serialize(input)
+        );
+        expect(result).toEqual(expected ?? input)
+    }
 });
